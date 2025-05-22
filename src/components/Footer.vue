04bfd30e9aef1415a38c068f5b1bce773e97b34c
maxhu08 <contact@maxhu.dev>
2025-05-22 10:58:15 -0600

    production-1

--- src/components/Footer.vue
+++
@@ -1,5 +0,0 @@
-<template>
-  <footer class="bg-gray-800 text-center text-white p-4">
-    <p>&copy; 2025 GameKeys. Todos los derechos reservados.</p>
-  </footer>
-</template>
--- src/views/NotFoundView.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div class=" bg-gray-900 text-center p-10 min-h-screen">
-      <h1 class="text-4xl font-bold text-yellow-500">404 - Página no encontrada</h1>
-      <p class="mt-4 text-white">Lo sentimos, la página que buscas no existe.</p>
-      <router-link to="/" class="text-blue-500 mt-4 inline-block border-2 p-5 mt-10">Volver al inicio</router-link>
-    </div>
-  </template>
-  
